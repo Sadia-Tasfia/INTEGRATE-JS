f57d53949b09682ae103c344ef62d4755be92320
Sadia-Tasfia <tasfia.sujana2903@gmail.com>
2023-11-01 00:37:55 +0600

    add more file

---
+++ try-catch.js
@@ -0,0 +1,19 @@
+const day = "monday";
+console.log(day);
+
+// add error code
+// kono code dekhe error mone hole seta try er moddhe diye dibe ..thla seta sevabe handle krbe
+
+try{
+  console.log(number)
+  let number = '23';
+  console.log("inside try");
+} 
+catch(error) {
+  console.log("inside catch");
+  // console.log(error);
+} 
+finally{
+  console.log("finally done");
+}
+console.log("last line of the code");
